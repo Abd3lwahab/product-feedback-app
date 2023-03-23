98ddad7eef81bd811392a20972bd5aa623510472
Abdelwahab Hussein <abdelwahab.huss07@gmail.com>
2023-03-24 00:06:33 +0200

    Add homepage components

---
+++ src/components/Roadmap/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Roadmap = (): React.ReactElement => {
+  return (
+    <div className="hidden md:block bg-white rounded-lg lg:mb-6 lg:w-[255px] md:flex-1">
+      <div className="flex flex-row justify-between items-center px-6 pt-4">
+        <h3 className="text-h3 text-blue-dark">Roadmap</h3>
+        {/* <Link
+          to={'/roadmap'}
+          title={'View'}
+          className={'text-blue text-body-3 font-semibold underline hover:opacity-60'}
+        >
+          View
+        </Link> */}
+      </div>
+      <div className="px-6 py-6">
+        <div className="flex flex-row items-center justify-between mb-2 text-body-1 text-blue-gray">
+          <div className="flex flex-row items-center">
+            <div className="w-2 h-2 bg-orange rounded-full mr-4"></div>
+            <span className="font-normal">Planned</span>
+          </div>
+          <span className="font-bold text-right">1</span>
+        </div>
+        <div className="flex flex-row items-center justify-between mb-2 text-body-1 text-blue-gray">
+          <div className="flex flex-row items-center">
+            <div className="w-2 h-2 bg-purple rounded-full mr-4"></div>
+            <span className="font-normal">In-Progress</span>
+          </div>
+          <span className="font-bold text-right">2</span>
+        </div>
+        <div className="flex flex-row items-center justify-between text-body-1 text-blue-gray">
+          <div className="flex flex-row items-center">
+            <div className="w-2 h-2 bg-cyan rounded-full mr-4"></div>
+            <span className="font-normal">Live</span>
+          </div>
+          <span className="font-bold text-right">3</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Roadmap;
---
+++ src/components/Suggestions/EmptyContent.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '../Button';
+import NoContentIcon from '../../assets/suggestions/illustration-empty.svg';
+
+function EmptyContent() {
+  return (
+    <div className="bg-white rounded-lg flex flex-col items-center justify-center mx-6 md:mx-0 mb-6">
+      <div className="py-[76px] md:py-[110px] w-[410px]">
+        <img src={NoContentIcon} className="w-[129px] m-auto" />
+        <div className="mt-14 text-center">
+          <h1 className="text-h1 text-blue-dark mb-4">There is no feedback yet.</h1>
+          <p className="text-blue-gray text-body-1 mb-12">
+            Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas
+            to improve our app.
+          </p>
+          <Button color={'purple'} text="+ Add Feedback" onClick={() => {}} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EmptyContent;
