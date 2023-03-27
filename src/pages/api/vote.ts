7be3316bc60f271c4a21c2b823ed8bf5c1525ae7
Abdelwahab Hussein <abdelwahab.huss07@gmail.com>
2023-03-27 17:36:58 +0200

    Refactor vote.ts to simplify validation and return only updated user in response

--- src/pages/api/vote.ts
+++ src/pages/api/vote.ts
@@ -1,17 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/lib/prisma';
-import { Feedback, User } from '@/types';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
       const { userId, feedbackId, action } = req.body;
 
-      const user: User = await prisma.user.findUnique({ where: { id: userId } });
-      const feedback: Feedback = await prisma.feedback.findUnique({ where: { id: feedbackId } });
+      const user = await prisma.user.findUnique({ where: { id: userId } });
 
-      if (!feedback || !user) {
-        return res.status(404).json({ message: 'Feedback or user not found' });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found.' });
       }
 
       if (action === 'upvote' && !user.upvoteFeedbackIDs.includes(feedbackId)) {
@@ -19,12 +17,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           where: { id: userId },
           data: { upvoteFeedbackIDs: { push: feedbackId } },
         });
-        const updatedFeedback = await prisma.feedback.update({
+        await prisma.feedback.update({
           where: { id: feedbackId },
           data: { upvotes: { increment: 1 } },
         });
 
-        return res.status(200).json({ user: updatedUser, feedback: updatedFeedback });
+        return res.status(200).json({ user: updatedUser });
       } else if (action === 'downvote' && user.upvoteFeedbackIDs.includes(feedbackId)) {
         const updatedUser = await prisma.user.update({
           where: { id: userId },
@@ -32,12 +30,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             upvoteFeedbackIDs: { set: user.upvoteFeedbackIDs.filter(id => id !== feedbackId) },
           },
         });
-        const updatedFeedback = await prisma.feedback.update({
+        await prisma.feedback.update({
           where: { id: feedbackId },
           data: { upvotes: { decrement: 1 } },
         });
 
-        return res.status(200).json({ user: updatedUser, feedback: updatedFeedback });
+        return res.status(200).json({ user: updatedUser });
       } else {
         return res.status(500).json({ message: 'Unknown action or repeated.' });
       }
