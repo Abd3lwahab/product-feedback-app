8c477ff6cba576c147b8fc0852e0ae33ccfd5024
Abdelwahab Hussein <abdelwahab.huss07@gmail.com>
2023-04-08 01:36:01 +0200

    fetch currentUser in layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,24 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect } from 'react';
 import { Jost } from 'next/font/google';
+import { User } from '@/types';
+import { currentUserState } from '@/atoms/currentUserAtom';
+import axios from 'axios';
+import { useRecoilState } from 'recoil';
 
 const jost = Jost({ subsets: ['latin'] });
 
 export default function Layout({ children }: PropsWithChildren) {
+  const [currentUser, setCurrentUser] = useRecoilState<User>(currentUserState);
+
+  async function fetchUser() {
+    const res = await axios.get('/api/user');
+    setCurrentUser(res.data);
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <main
