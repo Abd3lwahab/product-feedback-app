c5c80c5abaa5737c3b5d66d10063f92188ebd3b6
Abdelwahab Hussein <abdelwahab.huss07@gmail.com>
2023-04-08 02:52:05 +0200

    Add update feedback endpoint

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -3,13 +3,20 @@ import prisma from '@/lib/prisma';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
-    case 'POST':
-      return handlePOST(req, res);
     case 'GET':
       return handleGET(req, res);
+    case 'POST':
+      return handlePOST(req, res);
+    case 'PUT':
+      return handlePUT(req, res);
     default:
       return res.status(500).json({ error: `The HTTP ${req.method} method is not supported.` });
   }
+};
+
+const handleGET = async (req: NextApiRequest, res: NextApiResponse) => {
+  const result = await prisma.feedback.findMany();
+  return res.status(200).json(result);
 };
 
 const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -24,7 +31,15 @@ const handlePOST = async (req: NextApiRequest, res: NextApiResponse) => {
   return res.status(201).json(result);
 };
 
-const handleGET = async (req: NextApiRequest, res: NextApiResponse) => {
-  const result = await prisma.feedback.findMany();
-  return res.status(200).json(result);
+const handlePUT = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { feedbackId, data } = req.body;
+  const result = await prisma.feedback.update({
+    where: {
+      id: feedbackId,
+    },
+    data: data,
+  });
+  return res.status(200).json({
+    message: 'Feedback updated successfully',
+  });
 };
